ecc3242fe9f345927e7631ed166edc55c8fbf975
layla dantas <laylatdantas@gmail.com>
2019-05-17 06:10:59 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,48 +1,55 @@
 def welcome
  puts "Welcome to the Blackjack Table"
- # code #welcome here
+ 
 end
 
 def deal_card
   rand(1..11)
-    # code #deal_card here
+    
 end
 
 def display_card_total
-  card total == 7 
+ 
   
-  # code #display_card_total here
+ 
 end
 
 def prompt_user
- gets.chomp
-  # code #prompt_user here
+ 
+  
 end
 
 def get_user_input
-  # code #get_user_input here
+ 
+ 
+ 
+ 
 end
 
 def end_game
-  card total == 27 
   
   
   
-  # code #end_game here
+  
+  
 end
 
 def initial_round
   
   
-  # code #initial_round here
+  
 end
 
 def hit?
-  # code hit? here
+  
+  
+  
 end
 
 def invalid_command
-  # code invalid_command here
+ 
+ 
+ 
 end
 
 #####################################################
